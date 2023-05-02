2c35f787428cacfd7b5690952ce9b5b9f490dc17
michmattera <michellemattera97@gmail.com>
2023-05-02 16:43:54 +0000

    Create superuser

--- myproject/models.py
+++ myproject/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+# class Booking(models.model):
+#     """
+#     Model for booking private lesson
+#     User has to be sign in to book
+#     """
+#     client = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
