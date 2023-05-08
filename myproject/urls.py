6ad7b6a51adb3cfa617ea81f18d4e6663c6d1c49
michmattera <michellemattera97@gmail.com>
2023-05-08 17:00:43 +0000

    Personalize registration form and view

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
-from .views import SignUpView
+from .views import register
 
 
 urlpatterns = [
     path('', views.BookingListView.as_view(), name='booking'),
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path('register/', views.register, name="register"),
+    # path("signup/", SignUpView.as_view(), name="signup"),
+    # path("login/", LoginView.as_view(), name="login"),
     
 ]
--- myproject/views.py
+++ myproject/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import redirect, render
 from django.views import generic
 from django.contrib import messages
 from django.contrib.auth import forms
+from django.contrib.auth.models import User, auth
 from django.views.generic import ListView, FormView
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
 from .models import Booking
 
 
@@ -15,8 +14,36 @@ class BookingListView(generic.ListView):
     template_name = 'index.html'
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("home")
-    template_name = "signup.html"
+# class SignUpView(generic.CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy("home")
+#     template_name = "signup.html"
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
 
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is already taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password,
+                                        email=email)
+                user.save()
+                
+                return redirect('home')
+
+        else:
+            messages.info(request, 'Both passwords are not matching')
+            return redirect(register)
+            
+    else:
+        return render(request, 'register.html')
+
+# class LoginView(generic.)
