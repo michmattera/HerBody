43a62fac02529b2256f8cf6a98179a144fafaa31
michmattera <michellemattera97@gmail.com>
2023-06-20 17:51:10 +0000

    reinstall all libraries and env.py for issue with gitpod workspace

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.http import HttpResponse
-
 from . import views
 from . import models
 from .models import Booking
