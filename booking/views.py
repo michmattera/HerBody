2d6b0e2418bdbaa581099bb91ec51d09856ba7fc
michmattera <michellemattera97@gmail.com>
2023-07-18 17:33:10 +0000

    Fixed bug: change how the week was showing to eliminate more than 2 session bug

--- booking/views.py
+++ booking/views.py
@@ -12,16 +12,36 @@ from dateutil import parser
 from django.urls import reverse
 
 
+def get_week_start_end_dates(today_date):
+    days_until_tuesday = (today_date.weekday() - 1) % 7
+    start_of_week = today_date - timedelta(days=days_until_tuesday)
+    end_of_week = start_of_week + timedelta(days=6)
+    return start_of_week, end_of_week
+
+def get_week_start_date(today):
+    current_weekday = today.weekday()
+    days_to_tuesday = (current_weekday - 1) % 7
+    start_of_week = today - timedelta(days=days_to_tuesday)
+    return start_of_week
+
+
+def get_week_end_date(today):
+    current_weekday = today.weekday()
+    days_to_sunday = (6 - current_weekday) % 7
+    end_of_week = today + timedelta(days=days_to_sunday)
+    return end_of_week
+
 
 def get_available_slots():
     today_date = timezone.localdate()
-    end_of_week = today_date + timedelta(days=7)
-    booked_slots = Booking.objects.filter(date__range=[today_date, end_of_week]).values_list('date', 'time')
+    start_of_week, end_of_week = get_week_start_end_dates(today_date)
+    
+    booked_slots = Booking.objects.filter(date__range=[start_of_week, end_of_week]).values_list('date', 'time')
     available_slots = []
 
     time_choices = Booking._meta.get_field('time').choices
-    for day in range(1, 7):
-        current_date = today_date + timedelta(days=day)
+    for day in range(0, 6):
+        current_date = start_of_week + timedelta(days=day)
         time_slots = []
 
         for hour, label in time_choices:
@@ -37,30 +57,73 @@ def get_available_slots():
 
     return available_slots
 
+# def get_available_slots():
+#     today_date = timezone.localdate()
+#     end_of_week = today_date + timedelta(days=7)
+#     booked_slots = Booking.objects.filter(date__range=[today_date, end_of_week]).values_list('date', 'time')
+#     available_slots = []
+
+#     time_choices = Booking._meta.get_field('time').choices
+#     for day in range(1, 7):
+#         current_date = today_date + timedelta(days=day)
+#         time_slots = []
+
+#         for hour, label in time_choices:
+#             current_time = time(hour)
+#             current_slot = datetime.combine(current_date, current_time)
+#             if (current_date, hour) in booked_slots:
+#                 slot_status = 'Booked'
+#             else:
+#                 slot_status = 'Available'
+#             time_slots.append({'time': current_slot, 'status': slot_status})
+
+#         available_slots.append({'date': current_date, 'time_slots': time_slots})
+
+#     return available_slots
 
 class booking_list(generic.ListView):
     model = Booking
-    template_name = 'booking/my_bookings.html'  # Specify the template name
+    template_name = 'booking/my_bookings.html'
 
     def get_queryset(self):
         today = date.today()
-        end_of_week = today + timedelta(days=7)
-        queryset = self.model.objects.filter(user=self.request.user, date__range=[today, end_of_week])
+        start_of_week = get_week_start_date(today)
+        end_of_week = get_week_end_date(today)
 
-        # Check if there are session variables for updated booking
+        queryset = self.model.objects.filter(user=self.request.user, date__range=[start_of_week, end_of_week])
         new_date = self.request.session.get('new_date')
         new_time = self.request.session.get('new_time')
 
         if new_date and new_time:
-            # Create a new booking object with the updated date and time
             new_booking = Booking(date=new_date, time=new_time, user=self.request.user)
-            queryset |= QuerySet([new_booking])  # Add the updated booking to the queryset
-
-        # Clear the session variables after retrieving them
         self.request.session.pop('new_date', None)
         self.request.session.pop('new_time', None)
 
         return queryset
+
+# class booking_list(generic.ListView):
+#     model = Booking
+#     template_name = 'booking/my_bookings.html'  # Specify the template name
+
+#     def get_queryset(self):
+#         today = date.today()
+#         end_of_week = today + timedelta(days=7)
+#         queryset = self.model.objects.filter(user=self.request.user, date__range=[today, end_of_week])
+
+#         # Check if there are session variables for updated booking
+#         new_date = self.request.session.get('new_date')
+#         new_time = self.request.session.get('new_time')
+
+#         if new_date and new_time:
+#             # Create a new booking object with the updated date and time
+#             new_booking = Booking(date=new_date, time=new_time, user=self.request.user)
+#             queryset |= QuerySet([new_booking])  # Add the updated booking to the queryset
+
+#         # Clear the session variables after retrieving them
+#         self.request.session.pop('new_date', None)
+#         self.request.session.pop('new_time', None)
+
+#         return queryset
 
 
 
@@ -115,8 +178,7 @@ def booking_confirmation(request):
 def booking_form(request):
     user = request.user
     today = datetime.today().date()
-    start_of_week = today - timedelta(days=today.weekday())
-    end_of_week = start_of_week + timedelta(days=6)
+    start_of_week, end_of_week = get_week_start_end_dates(today)
 
     user_booking_count = Booking.objects.filter(user=user, date__range=[start_of_week, end_of_week]).count()
 
@@ -130,29 +192,6 @@ def booking_form(request):
     return render(request, "booking/booking_form.html", {'form': form, 'available_slots': available_slots})
 
 
-
-# @login_required
-# def edit_booking(request, booking_id):
-#     booking = get_object_or_404(Booking, id=booking_id)
-#     if request.method == 'POST':
-#         form = BookingForm(data=request.POST, instance=booking)
-#         if form.is_valid():
-#             new_date = form.cleaned_data['date']
-#             new_time = form.cleaned_data['time']
-#             date = new_date
-#             time = new_time
-#             form.save()
-#             return redirect('edit_booking_confirm', booking_id=booking_id)
-#     else:
-#         form = BookingForm(instance=booking)
-
-#     available_slots = get_available_slots()
-
-#     return render(request, 'booking/edit_booking.html', {
-#         'booking': booking,
-#         'form': form,
-#         'available_slots': available_slots,
-#     })
 @login_required
 def edit_booking(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
