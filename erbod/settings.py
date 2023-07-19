f354afd063d77c548cf091ed164cdbbf2c0f500f
michmattera <michellemattera97@gmail.com>
2023-07-19 22:30:43 +0000

    minor css change, change settings to fix deployment bug

--- erbod/settings.py
+++ erbod/settings.py
@@ -105,15 +105,16 @@ WSGI_APPLICATION = 'erbod.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+# for testing purpose
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
