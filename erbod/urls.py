a49e93fd2428b5962aa4a8ada630eac0bd6ecdbb
michmattera <michellemattera97@gmail.com>
2023-07-21 19:11:10 +0000

    404 error

--- erbod/urls.py
+++ erbod/urls.py
@@ -5,6 +5,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myproject.urls'), name='myproject_urls'),
-    path('', TemplateView.as_view(template_name="index.html"), name="home"),
+    # path('', TemplateView.as_view(template_name="index.html"), name="home"),
     path('booking/', include('booking.urls'), name='booking_urls'),
 ]
